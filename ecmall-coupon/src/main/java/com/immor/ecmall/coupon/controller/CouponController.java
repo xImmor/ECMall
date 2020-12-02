49d0dc4718cf51c3d2d47031ea2681592fbbb739
immor <1125584876@qq.com>
2020-12-02 17:34:53 +0800

    spring cloud alibaba nacos config 配置中心

--- ecmall-coupon/src/main/java/com/immor/ecmall/coupon/controller/CouponController.java
+++ ecmall-coupon/src/main/java/com/immor/ecmall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,23 @@ import com.immor.common.utils.R;
  * @email shenxian@gmail.com
  * @date 2020-12-02 10:26:13
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
 
     /**
      * openFeign远程调用测试接口
