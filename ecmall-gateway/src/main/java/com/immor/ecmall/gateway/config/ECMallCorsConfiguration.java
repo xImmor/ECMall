2c3f709b3d08b10638465edb89c20ff2f58af172
immor <1125584876@qq.com>
2020-12-08 13:57:20 +0800

    网关跨域配置

---
+++ ecmall-gateway/src/main/java/com/immor/ecmall/gateway/config/ECMallCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.immor.ecmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Author: immor
+ * @Date: 2020/12/8
+ */
+@Configuration
+public class ECMallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
--- ecmall-product/src/main/java/com/immor/ecmall/product/service/impl/CategoryServiceImpl.java
+++ ecmall-product/src/main/java/com/immor/ecmall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,14 @@
 package com.immor.ecmall.product.service.impl;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +23,9 @@ import com.immor.ecmall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+    /*@Autowired
+    private CategoryDao categoryDao;*/
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -24,6 +34,40 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        /*组装成父子结构树
+         * 1）获得所有一级分类
+         * */
+        List<CategoryEntity> entitiesLev1 = entities.stream().filter(item -> item.getParentCid() == 0)
+                .map(item -> {
+                    item.setChildren(getChildrens(item, entities));
+                    return item;
+                }).sorted(Comparator.comparingInt(item -> (item.getSort() == null ? 0 : item.getSort()))).collect(Collectors.toList());
+
+        return entitiesLev1;
+    }
+
+    /**
+     * 递归查找所有菜单的子菜单
+     *
+     * @param root
+     * @param all
+     * @return
+     */
+    public List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> result = all.stream().filter(menu ->
+            menu.getParentCid() == root.getCatId()
+        ).map(menu -> {
+            menu.setChildren(getChildrens(menu, all));
+            return menu;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+        return result;
     }
 
 }
