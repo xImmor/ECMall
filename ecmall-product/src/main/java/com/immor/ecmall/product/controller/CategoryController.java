4b1c5c0b79758682b778c049374ea504f18548ed
immor <1125584876@qq.com>
2020-12-24 15:07:53 +0800

    批量删除接口

--- ecmall-product/src/main/java/com/immor/ecmall/product/controller/CategoryController.java
+++ ecmall-product/src/main/java/com/immor/ecmall/product/controller/CategoryController.java
@@ -75,6 +75,13 @@ public class CategoryController {
         return R.ok();
     }
 
+    @RequestMapping("/update/sort")
+    //@RequiresPermissions("product:category:update")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
+        return R.ok();
+    }
+
     /**
      * 删除
      */
