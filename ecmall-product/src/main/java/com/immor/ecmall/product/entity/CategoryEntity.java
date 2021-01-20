cae78c83a34be25626bf970c57677f652455f290
immor <1125584876@qq.com>
2021-01-20 15:51:43 +0800

    JsonInclude

--- ecmall-product/src/main/java/com/immor/ecmall/product/entity/CategoryEntity.java
+++ ecmall-product/src/main/java/com/immor/ecmall/product/entity/CategoryEntity.java
@@ -3,12 +3,11 @@ package com.immor.ecmall.product.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
-
-import lombok.Data;
 
 /**
  * ??Ʒ???????
@@ -60,6 +59,7 @@ public class CategoryEntity implements Serializable {
 	 */
 	private Integer productCount;
 
+	@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
 	@TableField(exist = false)
 	private List<CategoryEntity> children;
 
